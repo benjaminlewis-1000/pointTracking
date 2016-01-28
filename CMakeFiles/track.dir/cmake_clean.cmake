11febf3045da119f429ab1ecb0c1d1d72c0cc53c
Ben <speedyswimmer1000@gmail.com>
2016-01-28 15:27:17 -0700

    Added video

---
+++ CMakeFiles/track.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/track.dir/pointTrack.cpp.o"
+  "track.pdb"
+  "track"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/track.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- pointTrack.cpp
+++ pointTrack.cpp
@@ -3,3 +3,29 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 
+#include <stdlib.h>
+
+using namespace cv;
+
+volatile int quit_signal=0;
+#ifdef __unix__
+#include <signal.h>
+extern "C" void quit_signal_handler(int signum) {
+ if (quit_signal!=0) exit(0); // just exit already
+ quit_signal=1;
+// printf("Will quit at next camera frame (repeat to kill now)\n");
+}
+#endif
+
+int main(int argc, char** argv){
+	namedWindow("Video",WINDOW_NORMAL);
+	
+	VideoCapture cap(argv[1]); // open the default camera
+	if(!cap.isOpened()){ // check if we succeeded
+		return -1;
+	}
+	#ifdef __unix__
+	   signal(SIGINT,quit_signal_handler); // listen for ctrl-C
+	#endif
+
+}
